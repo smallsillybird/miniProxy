f1dac92ebf70949a8635c9ff1a4f14540d8b84e3
smallsillybird <91230904+smallsillybird@users.noreply.github.com>
2021-10-12 20:17:26 +0800

    Update miniProxy.php

--- miniProxy.php
+++ miniProxy.php
@@ -54,7 +54,7 @@ $startURL = "";
 //When no $startURL is configured above, miniProxy will show its own landing page with a URL form field
 //and the configured example URL. The example URL appears in the instructional text on the miniProxy landing page,
 //and is proxied when pressing the 'Proxy It!' button on the landing page if its URL form is left blank.
-$landingExampleURL = "https://example.net";
+$landingExampleURL = "https://www.google.com.hk/";
 
 /****************************** END CONFIGURATION ******************************/
 
